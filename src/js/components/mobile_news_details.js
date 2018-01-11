ddb068259891c5f17cc5b0233edeb6373ed7e52e
Mario <zhangbiaoFT@163.com>
2018-01-11 14:48:09 +0800

    移动端详情页开发

---
+++ src/js/components/mobile_news_details.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Row, Col,BackTop} from 'antd';
+import MobileHeader from './mobile_header';
+import MobileFooter from './mobile_footer';
+export default class MobileNewsDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newsItem: ''
+    };
+  };
+  componentDidMount() {
+    var myFetchOptions = {
+      method: 'GET'
+    };
+    console.log(this.props.match.params);
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey="
+      + this.props.match.params.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
+      this.setState({newsItem: json});
+      document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
+    })
+  };
+  createMarkup() {
+    return {__html: this.state.newsItem.pagecontent};
+  };
+  render() {
+    return (
+      <div id="mobileDetailsContainer">
+        <MobileHeader/>
+        <div className="ucmobileList">
+          <Row>
+            <Col span={24}>
+              <div class="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
+            </Col>
+          </Row>
+        </div>
+        <MobileFooter/>
+        <BackTop/>
+      </div>
+    );
+  };
+}
