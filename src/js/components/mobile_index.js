31691926281683beee4c23debc8238eb6b0c3ffd
Mario <zhangbiaoFT@163.com>
2018-01-11 09:18:39 +0800

    移动端图片轮播

--- src/js/components/mobile_index.js
+++ src/js/components/mobile_index.js
@@ -2,15 +2,31 @@ import React from 'react';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
 import MobileList from './mobile_list';
-import {Tabs} from 'antd';
+import {Tabs, Carousel} from 'antd';
 const TabPane = Tabs.TabPane;
 export default class MobileIndex extends React.Component{
     render() {
+      // 轮播图设置
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        autoplay: true
+      }
       return(
         <div>
           <MobileHeader/>
           <Tabs>
             <TabPane tab="头条" key="1">
+              <div class="carousel">
+                <Carousel {...settings}>
+                  <div><img src="./src/images/carousel_1.jpg"/></div>
+                  <div><img src="./src/images/carousel_2.jpg"/></div>
+                  <div><img src="./src/images/carousel_3.jpg"/></div>
+                  <div><img src="./src/images/carousel_4.jpg"/></div>
+                </Carousel>
+              </div>
               <MobileList count={20} type="top"/>
             </TabPane>
             <TabPane tab="社会" key="2">
